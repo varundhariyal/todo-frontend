b22d85b1abbbfb50e8e8c8a981c0b2e25c297e80
varundhariyal <varundhariyal@gmail.com>
2019-06-05 01:27:31 +0530

    sokcet io url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: 'http://api.livetodo.xyz/v1/',
-  socketUrl: 'http://api.livetodo.xyz/v1'
+  socketUrl: 'http://api.livetodo.xyz'
 
 
 };
